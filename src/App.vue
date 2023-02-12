761c29e91c3b6188e5855858ac9c5783912d4c28
goodgay897 <119532864+goodgay897@users.noreply.github.com>
2023-02-12 03:35:09 +0300

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   src/App.vue
    	modified:   src/components/task.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <ul>
-    <task v-for="task in tasks" :title="task.title" :done="task.done" :_id="task._id"
-      :being-edited="task.beingEdited" 
-      
+    <task
+      v-for="task in tasks"
+      :title="task.title"
+      :done="task.done"
+      :_id="task._id"
+      :being-edited="task.beingEdited"
       @remove="remove(task)"
-      @edit="edit(task)" 
+      @edit="edit(task)"
       @check="check(task, $event)"
-      @save="save(task, $event)"/>
-
-
+      @save="save(task, $event)"
+    />
   </ul>
   <form @submit.prevent="add" class="add">
-    <input type="text"/>    
+    <input type="text" />
     <button type="submit">add</button>
   </form>
-  
+
   <!-- <button @click="add">add</button> -->
 </template>
 
@@ -46,40 +48,42 @@ function remove() {
 
 </script> -->
 
-
-
 <script lang="ts">
-import task from './components/task.vue';
-import axios from 'axios';
+import task from "./components/task.vue";
+import axios from "axios";
 
 export default {
   components: { task },
   data() {
     return {
-      tasks: [] as Array<{ title: string, done: boolean, _id: string, beingEdited: boolean }>
-    }
+      tasks: [] as Array<{
+        title: string;
+        done: boolean;
+        _id: string;
+        beingEdited: boolean;
+      }>,
+    };
   },
   beforeMount() {
     this.sync();
   },
   methods: {
-
     add(event: SubmitEvent) {
-      if(event.target[0].value === "") return;
-      axios.post("http://127.0.0.1:8000/items/create", 
-      {
-        title: event.target[0].value
-      })
-      .then((response) => {
-        this.tasks.push(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      if (event.target[0].value === "") return;
+      axios
+        .post("http://127.0.0.1:8000/items/create", {
+          title: event.target[0].value,
+        })
+        .then((response) => {
+          this.tasks.push(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
       event.target.reset();
     },
 
-    edit(task: { title: string, done: boolean, _id: string, beingEdited: boolean }) {
+    edit(task: { title: string; done: boolean; _id: string; beingEdited: boolean }) {
       this.tasks.forEach((t) => {
         if (t._id !== task._id) {
           t.beingEdited = false;
@@ -87,53 +91,57 @@ export default {
       });
       task.beingEdited = true;
     },
-    remove(task: { title: string, done: boolean, _id: string, beingEdited: boolean }) {
-      axios.post("http://127.0.0.1:8000/items/delete", 
-      {
-        _id: task._id
-      }).then((response) => {
-        this.sync();
-      }).catch((error) => {
-        console.log(error);
-      });
+    remove(task: { title: string; done: boolean; _id: string; beingEdited: boolean }) {
+      axios
+        .post("http://127.0.0.1:8000/items/delete", {
+          _id: task._id,
+        })
+        .then((response) => {
+          this.sync();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
       this.sync();
-
     },
-    check(task: { title: string, done: boolean, _id: string, beingEdited: boolean }, event: Event) {
-      axios.post("http://127.0.0.1:8000/items/update", 
-      {
-        _id: task._id,
-        done: (event.target as HTMLInputElement).checked
-      }).then((response) => {
-        this.sync();
-      })
+    check(
+      task: { title: string; done: boolean; _id: string; beingEdited: boolean },
+      event: Event
+    ) {
+      axios
+        .post("http://127.0.0.1:8000/items/update", {
+          _id: task._id,
+          done: (event.target as HTMLInputElement).checked,
+        })
+        .then((response) => {
+          this.sync();
+        });
     },
-    save(task: { title: string, done: boolean, _id: string, beingEdited: boolean }, event: SubmitEvent) {
-      axios.post("http://127.0.0.1:8000/items/update", 
-      {
-        _id: task._id,
-        title: event.target[0].value
-      }).then((response) => {
-        this.sync();
-      })
+    save(
+      task: { title: string; done: boolean; _id: string; beingEdited: boolean },
+      event: SubmitEvent
+    ) {
+      axios
+        .post("http://127.0.0.1:8000/items/update", {
+          _id: task._id,
+          title: event.target[0].value,
+        })
+        .then((response) => {
+          this.sync();
+        });
     },
-    sync(){
-      axios.get('http://127.0.0.1:8000/items')
-      .then((response) => {
-        this.tasks = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    }
-
-
-  }
-  
-}
-
+    sync() {
+      axios
+        .get("http://127.0.0.1:8000/items")
+        .then((response) => {
+          this.tasks = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
-  
-<style>
 
-</style>
+<style></style>
--- src/components/task.vue
+++ src/components/task.vue
@@ -1,38 +1,37 @@
 <template>
-    <div :class="'task' + (done ? ' done' : '')">
-        <input class="checkbox" type="checkbox" :checked="done ? true : false" @change="$emit('check', $event)"/>
+  <div :class="'task' + (done ? ' done' : '')">
+    <input
+      class="checkbox"
+      type="checkbox"
+      :checked="done ? true : false"
+      @change="$emit('check', $event)"
+    />
 
+    <div class="title" v-if="!beingEdited">{{ title }}</div>
+    <button @click="$emit('edit')" v-if="!beingEdited">edit</button>
 
-        <div class="title" v-if="!beingEdited">{{ title }}</div>
-        <button @click="$emit('edit')" v-if="!beingEdited">edit</button>
+    <form @submit.prevent="$emit('save', $event)" class="add" v-if="beingEdited">
+      <input class="title" :value="title" type="text" />
+      <button type="submit">save</button>
+    </form>
 
-        <form @submit.prevent="$emit('save', $event)" class="add" v-if="beingEdited"> 
-          <input class="title" :value="title" type="text" />        
-          <button type="submit">save</button>
-        </form>
-
-        
-        
-
-        <button @click="$emit('remove')" class='remove'>X</button>
-    </div>
+    <button @click="$emit('remove')" class="remove">X</button>
+  </div>
 </template>
-
 
 <script lang="ts">
 export default {
-	props: ['title','done','beingEdited', 'save'],
-  emits: ['remove', 'edit', 'save', 'check'],
+  props: ["title", "done", "beingEdited", "save"],
+  emits: ["remove", "edit", "save", "check"],
   data() {
-    return {
-    }
+    return {};
   },
   // methods: {
   //   edit(state: boolean) {
   //     this.beingEdited = state;
   //   }
   // }
-}
+};
 </script>
 
 <style>
